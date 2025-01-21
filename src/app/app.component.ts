5d5e8f463818d2f34870ac497a560ba33d7dafae
ayanbisi inioluwa <ifeini048@gmail.com>
2025-01-21 12:35:17 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-project';
+  num1: number = 5;
+  num2: number = 2;
+  name = 'inioluwa'
+  day = 'Sunday'
+  toggleDays() {
+    
+  }
+
+  iSeeFood = false;
+  username = ""
+  myFuntion() {
+    console.log()
+  }
 }
