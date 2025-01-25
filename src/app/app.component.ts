2b912fa23b0d1f4a8be07eb808a49f8557b088d7
ayanbisi inioluwa <ifeini048@gmail.com>
2025-01-25 05:33:50 +0100

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,65 @@ export class AppComponent {
   num1: number = 5;
   num2: number = 2;
   name = 'inioluwa'
-  day = 'Sunday'
+  day = 'Tuesday'
   toggleDays() {
-    
+    if (this.day == 'Monday') {
+      console.log('it is Monday');
+    } else if (this.day == 'Tuesday') {
+      console.log('it is Tuesday');
+    } else {
+      console.log('Today is not Monday');
+    }
   }
 
   iSeeFood = false;
   username = ""
-  myFuntion() {
+  myFunction() {
     console.log()
   }
+
+  condition = true;
+
+  products = [
+    {
+      id: 1,
+      name: "Toothbrush",
+      price: 1000,
+      available:true
+    },
+    {
+      id: 2,
+      name: "toothpaste",
+      price: 700,
+      available:false
+    },
+    {
+      id: 3,
+      name: "shoe polish",
+      price: 1000,
+      available:false
+    },
+    {
+      id: 4,
+      name: "T-shirt",
+      price: 7000,
+      available:true
+    },
+    {
+      id: 5,
+      name: "belt",
+      price: 2000,
+      available:true
+    },
+  ]
+
+  cart: any = [];
+  addToCart(product: any) {
+    this.cart.push(product);
+    console.log(this.cart);
+  }
+
+
+  condition1 = true;
+  condition2 = false;
 }
